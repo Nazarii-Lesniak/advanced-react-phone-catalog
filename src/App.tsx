708c9703933979b0b33057b9465141c2ac71ff7d
Nazarii-Lesniak <naziksay@gmail.com>
2026-05-15 19:09:00 +0300

    refactor: enable React Router v7 future flags and implement code splitting
    - Add v7_startTransition and v7_relativeSplatPath flags to HashRouter
    - Introduce React.lazy and Suspense for dynamic route splitting
    - Implement global ErrorBoundary component to catch runtime page crashes

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,94 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 import './App.scss';
 import { HomePage } from './modules/HomePage';
-import { NotFoundPage } from './modules/NotFoundPage';
 import { Layout } from './components/Layout';
 import { ThemeProvider } from './context/ThemeContext';
-import { CartPage } from './modules/CartPage';
 import { CartProvider } from './context/CartContext';
 import { FavouritesProvider } from './context/FavoritesContext';
-import { FavouritesPage } from './modules/FavouritesPage';
-import { CategoryPage } from './modules/CategoryPage';
 import { CategoriesType, PathType } from './types/Types';
-import { ProductDetailsPage } from './modules/ProductDetailsPage';
+import { Loader } from './components/Loader';
+import { ErrorBoundary } from './components/ErrorBoundary';
+
+const FavouritesPage = lazy(() =>
+  import('./modules/FavouritesPage').then(m => ({ default: m.FavouritesPage })),
+);
+
+const CategoryPage = lazy(() =>
+  import('./modules/CategoryPage').then(m => ({ default: m.CategoryPage })),
+);
+
+const CartPage = lazy(() =>
+  import('./modules/CartPage').then(m => ({ default: m.CartPage })),
+);
+
+const ProductDetailsPage = lazy(() =>
+  import('./modules/ProductDetailsPage').then(m => ({
+    default: m.ProductDetailsPage,
+  })),
+);
+
+const NotFoundPage = lazy(() =>
+  import('./modules/NotFoundPage').then(m => ({ default: m.NotFoundPage })),
+);
 
 export const App = () => (
   <div className="App">
     <ThemeProvider>
       <CartProvider>
         <FavouritesProvider>
-          <HashRouter>
-            <Routes>
-              <Route path={PathType.HOME} element={<Layout />}>
-                <Route index element={<HomePage />} />
+          <HashRouter
+            future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+          >
+            <ErrorBoundary>
+              <Suspense fallback={<Loader />}>
+                <Routes>
+                  <Route path={PathType.HOME} element={<Layout />}>
+                    <Route index element={<HomePage />} />
 
-                <Route
-                  path={PathType.PHONES}
-                  element={
-                    <CategoryPage
-                      title={'Mobile phones'}
-                      category={CategoriesType.PHONES}
+                    <Route
+                      path={PathType.PHONES}
+                      element={
+                        <CategoryPage
+                          title={'Mobile phones'}
+                          category={CategoriesType.PHONES}
+                        />
+                      }
                     />
-                  }
-                ></Route>
-                <Route
-                  path={PathType.TABLETS}
-                  element={
-                    <CategoryPage
-                      title={'Tablets'}
-                      category={CategoriesType.TABLETS}
+                    <Route
+                      path={PathType.TABLETS}
+                      element={
+                        <CategoryPage
+                          title={'Tablets'}
+                          category={CategoriesType.TABLETS}
+                        />
+                      }
                     />
-                  }
-                ></Route>
-                <Route
-                  path={PathType.ACCESSORIES}
-                  element={
-                    <CategoryPage
-                      title={'Accessories'}
-                      category={CategoriesType.ACCESSORIES}
+                    <Route
+                      path={PathType.ACCESSORIES}
+                      element={
+                        <CategoryPage
+                          title={'Accessories'}
+                          category={CategoriesType.ACCESSORIES}
+                        />
+                      }
                     />
-                  }
-                ></Route>
 
-                <Route
-                  path={PathType.FAVOURITES}
-                  element={<FavouritesPage />}
-                ></Route>
-                <Route path={PathType.CART} element={<CartPage />}></Route>
-                <Route
-                  path="/product/:productId"
-                  element={<ProductDetailsPage />}
-                />
+                    <Route
+                      path={PathType.FAVOURITES}
+                      element={<FavouritesPage />}
+                    />
+                    <Route path={PathType.CART} element={<CartPage />} />
+                    <Route
+                      path="/product/:productId"
+                      element={<ProductDetailsPage />}
+                    />
 
-                <Route path="*" element={<NotFoundPage />}></Route>
-              </Route>
-            </Routes>
+                    <Route path="*" element={<NotFoundPage />} />
+                  </Route>
+                </Routes>
+              </Suspense>
+            </ErrorBoundary>
           </HashRouter>
         </FavouritesProvider>
       </CartProvider>
